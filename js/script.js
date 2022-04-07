78d64580aabec403460c5fb029a5aa4c8fb6646d
kuchingneko28 <kuchingneko28@gmail.com>
2022-04-08 06:08:21 +0800

    add hidden scroll

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const navLinks = document.querySelector(".navbar__links");
 const burger = document.querySelector(".burger");
+const body = document.querySelector("body");
 burger.addEventListener("click", () => {
   navLinks.classList.toggle("navbar__links__active");
   burger.classList.toggle("navbar__links__active");
+  body.classList.toggle("hidden");
 });
