45ce4d8cb30d0435f98daea2c2e08ca53e872539
kuchingneko28 <kuchingneko28@gmail.com>
2022-04-25 16:11:58 +0800

    edit dikit-dikit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
+const navbar = document.querySelector(".navbar");
 const navLinks = document.querySelector(".navbar__links");
 const burger = document.querySelector(".burger");
 const body = document.querySelector("body");
 burger.addEventListener("click", () => {
   navLinks.classList.toggle("navbar__links__active");
   burger.classList.toggle("navbar__links__active");
-  body.classList.toggle("hidden");
+
+  if (body.classList.contains("hidden")) {
+    body.classList.remove("hidden");
+    body.classList.add("show");
+  } else {
+    body.classList.add("hidden");
+    body.classList.remove("show");
+  }
 });
+
+window.onscroll = function () {
+  if (window.pageYOffset > 10) {
+    navbar.classList.add("shadow");
+  } else {
+    navbar.classList.remove("shadow");
+  }
+};
