d90548070d422ed71e157848bbe3b4dd92a3e444
kuchingneko28 <kuchingneko28@gmail.com>
2022-04-25 22:15:04 +0800

    edit lagi

--- js/script.js
+++ js/script.js
@@ -2,20 +2,37 @@ const navbar = document.querySelector(".navbar");
 const navLinks = document.querySelector(".navbar__links");
 const burger = document.querySelector(".burger");
 const body = document.querySelector("body");
+const sections = document.querySelectorAll("section");
+const navLink = document.querySelectorAll(".navbar__links a");
+
+// Burger link
 burger.addEventListener("click", () => {
   navLinks.classList.toggle("navbar__links__active");
   burger.classList.toggle("navbar__links__active");
-
-  if (body.classList.contains("hidden")) {
-    body.classList.remove("hidden");
-    body.classList.add("show");
-  } else {
-    body.classList.add("hidden");
-    body.classList.remove("show");
-  }
 });
 
 window.onscroll = function () {
+  // Highlight link
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (window.pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+      console.log(current);
+    }
+
+    navLink.forEach((li) => {
+      console.log(li);
+      li.classList.remove("navbar__highlight");
+      if (li.classList.contains(current)) {
+        li.classList.add("navbar__highlight");
+      }
+    });
+  });
+
+  // Shadow navbar
   if (window.pageYOffset > 10) {
     navbar.classList.add("shadow");
   } else {
