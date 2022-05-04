3833aa29cf6dcbe466d3867711f388affd10556c
kuchingneko28 <kuchingneko28@gmail.com>
2022-05-04 19:33:09 +0800

    update dikit

--- js/script.js
+++ js/script.js
@@ -63,6 +63,10 @@ $(".owl-carousel").owlCarousel({
   loop: true,
   responsiveClass: true,
   responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
     300: {
       items: 2,
       nav: false,
